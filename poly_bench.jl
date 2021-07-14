b8509c6bc669ad41d04141fb0c039949600d4723
Martin Johnsrud <martin.johnsrud@gmail.com>
2021-07-14 13:15:25 +0200

    benchmark different ways to declare functions

--- poly_bench.jl
+++ poly_bench.jl
@@ -1,13 +1,16 @@
 using BenchmarkTools, Polynomials, ForwardDiff
 
+##
+
 const cnst = [
     -10.5       0.074       -6.96e-5    ;
     0.668e-3    -1.78e-5    2.80e-8     ;
     0.494e-6    -8.86e-10   0           ;
-]
-const p1 = Polynomial(cnst[1, end:-1:1])
-const p2 = Polynomial(cnst[2, end:-1:1])
-const p3 = Polynomial(cnst[3, end:-1:1])
+    ]
+const p1 = Polynomial(cnst[1:end, 1])
+const p2 = Polynomial(cnst[1:end, 2])
+const p3 = Polynomial(cnst[1:end, 3])
+
 
 @inline function f(T::Real, C::Real)
     fact = 1e-4
@@ -15,16 +18,33 @@ const p3 = Polynomial(cnst[3, end:-1:1])
 end
 
 ##
+
 param = [
     -10.5       0.074       -6.96e-5    ;
     0.668e-3    -1.78e-5    2.80e-8     ;
     0.494e-6    -8.86e-10   0           ;
-]
-q1 = Polynomial(cnst[1, end:-1:1])
-q2 = Polynomial(cnst[2, end:-1:1])
-q3 = Polynomial(cnst[3, end:-1:1])
+    ]
+    q1 = Polynomial(cnst[1:end, 1])
+    q2 = Polynomial(cnst[1:end, 2])
+    q3 = Polynomial(cnst[1:end, 3])
 
 @inline function g(T::Real, C::Real)
+    fact = 1e-4
+    return fact * C * (q1(C) + q2(C) * T + q3(C) * T^2)^2
+end
+
+##
+    
+@inline function h(T::Real, C::Real)
+    param = [
+    -10.5       0.074       -6.96e-5    ;
+    0.668e-3    -1.78e-5    2.80e-8     ;
+    0.494e-6    -8.86e-10   0           ;
+    ]
+    q1 = Polynomial(cnst[1:end, 1])
+    q2 = Polynomial(cnst[1:end, 2])
+    q3 = Polynomial(cnst[1:end, 3])
+
     fact = 1e-4
     return fact * C * (q1(C) + q2(C) * T + q3(C) * T^2)^2
 end
@@ -55,8 +75,10 @@ C = get_ad_array(n, m, 1; vals=v1)
 T = get_ad_array(n, m, 2; vals=v2);
 
 ##
-@benchmark x .= f.(v1, v2)
+@time x .= f.(v1, v2);
 ##
-@benchmark y .= f.(C, T)
+@time y .= f.(C, T);
 ##
-@benchmark y .= g.(C, T)
+@time y .= g.(C, T);
+##
+@time y .= h.(C, T);
